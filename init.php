dfd679ea3e2fd5916805b91caeee641f573cb97f
dat tran <tranthanhdat.it@gmail.com>
2025-02-13 17:35:12 +0700

    Version bump

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 Plugin Name: BFX crypto map
 Plugin URI: https://bitfinex.com
 description: BFX crypto map
-Version: 1.5.3
+Version: 1.5.4
 Author: BFX
 Author URI: https://bitfinex.com
 License: GPL2
